d1e6a8444d932b67cf17f59cea5a28255fa572bb
taojy123 <taojy123@163.com>
2017-10-20 15:22:00 +0800

    add test page

---
+++ pages/test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>hello {{ world }}</h1>
+    <button v-on:click="change">change</button>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        world: 'world!'
+      }
+    },
+    methods: {
+      change () {
+        this.world = 'nuxt!'
+      }
+    }
+  }
+</script>
+
